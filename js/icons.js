01f7f4ca6870a1dedbf7c6882fb3c5ae8684bf9b
bruinen <max.machnik@gmail.com>
2019-01-24 20:55:27 +0100

    Ipad fix try 1

--- js/icons.js
+++ js/icons.js
@@ -2,12 +2,15 @@ const allIcons = document.querySelectorAll('.nav__circle');
 allIcons.forEach(icon => {
     if(icon.dataset.iconname) {
         icon.classList.add(`nav__circle--${icon.dataset.iconname}`);
-        icon.style = `background-image: url('img/icons/${icon.dataset.iconname}.svg')`;
-        icon.addEventListener('mouseover', ()=> {
-            icon.style = `background-image: url('img/icons/${icon.dataset.iconname}_active.svg')`;
-        });
-        icon.addEventListener('mouseout', () => {
-            icon.style = `background-image: url('img/icons/${icon.dataset.iconname}.svg')`;
-        })
+        icon.querySelector('a').innerHTML = `
+            <img src=img/icons/${icon.dataset.iconname}_active.svg  class="nav__icon--active"/>
+            <img src=img/icons/${icon.dataset.iconname}.svg  class="nav__icon"/>
+            `
+        // icon.addEventListener('mouseover', ()=> {
+        //     icon.style = `background-image: url('img/icons/${icon.dataset.iconname}_active.svg')`;
+        // });
+        // icon.addEventListener('mouseout', () => {
+        //     icon.style = `background-image: url('img/icons/${icon.dataset.iconname}.svg')`;
+        // })
     }
 })
